d97003c44354fd18f3b71dd6841148099014a815
christikaes <christikaes@gmail.com>
2019-08-31 10:21:33 -0400

    Remove public path

--- docs/js/chunk-2d0e145d.7cb11e3c.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e145d"],{"7a74":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("v-footer",{staticClass:"justify-center",attrs:{height:"88"}},[n("social-media",{attrs:{large:""}})],1)},c=[],s={components:{SocialMedia:function(){return n.e("chunk-2d0c22aa").then(n.bind(null,"48c3"))}}},i=s,l=n("2877"),r=Object(l["a"])(i,a,c,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0e145d.7cb11e3c.js.map
